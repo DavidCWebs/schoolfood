02fb657c89b48bb226d923a704abb907d95a4b93
root <root@server.carawebs.com>
2014-04-30 18:52:58 +0000

    modified:   functions.php

--- functions.php
+++ functions.php
@@ -16,5 +16,6 @@ require_once locate_template('/lib/comments.php');        // Custom comments mod
 require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
-include_once('advanced-custom-fields/acf.php' );          // Set up ACF 
+include_once('advanced-custom-fields/acf.php' );          // Set up ACF
+include_once('acf-repeater/acf-repeater.php' );           // Set up ACF Repeater Fields
 require_once locate_template('/lib/custom.php');          // Custom functions
