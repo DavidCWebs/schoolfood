0d1a0ca99af1ebec21388fe55ab634fd5237fec6
DavidCWebs <david@carawebs.com>
2014-05-19 17:35:49 +0100

    Removed unecessary Google Fonts

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5ad8833f48d20859f0663f39c206b9b8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bdf2478f7d937b034ec55869d9e8d30f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
